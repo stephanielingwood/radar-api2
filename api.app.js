51808045e9a8be1d726c63c4e147fcf2d181df2b
Lekha Bhargavi <lekha@shippable.com>
2015-06-23 16:56:15 -0700

    Update api.app.js

--- api.app.js
+++ api.app.js
@@ -13,6 +13,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Accept,Content-Type,Authorization,Cookie');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 routes(app);
 
