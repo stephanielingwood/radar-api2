e1af4ab4598a0e47436521b1a25583891ba6298a
kailevy <kai0levy@gmail.com>
2015-06-23 16:38:01 -0700

    Cleaning

--- api.app.js
+++ api.app.js
@@ -1,10 +1,7 @@
-var debug = require('debug')('myapp:server');
-var http = require('http');
 var cors = require('cors');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/routes');
@@ -13,15 +10,7 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-if (process.env.WWW_URL)
-  var WWW_URL = process.env.WWW_URL;
-else if (process.env.WWW_PORT)
-  var WWW_URL = 'http://localhost:' + process.env.WWW_PORT;
-else
-  var WWW_URL = 'http://localhost:3000';
 
 app.use(cors());
 
