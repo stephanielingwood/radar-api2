ec335f5bb44e6610aff08c3b43d257a653b32aad
kailevy <kai0levy@gmail.com>
2015-08-14 16:16:26 -0700

    Update to have info page display build number properly

--- routes/info.js
+++ routes/info.js
@@ -1,15 +1,22 @@
 module.exports = info;
 var pjson = require('../package.json');
+var fs = require('fs');
 
 function info(req, res, next) {
+  var buildnum = 'unavailable';
   logger.info('Info page');
-  res.status(200).json({
-    version: pjson.version,
-    time: new Date(),
-    buildnumber: process.env.BUILDNUM || 'unavailable',
-    body: req.body,
-    query: req.query,
-    params: req.params,
-    method: req.method
+  fs.readFile('./config.txt', 'utf8', function (err, data) {
+    if (err)
+      console.log(err);
+    if (data) buildnum = data.replace('\n','');
+    res.status(200).json({
+        version: pjson.version,
+        time: new Date(),
+        buildnumber: buildnum,
+        body: req.body,
+        query: req.query,
+        params: req.params,
+        method: req.method
+    });
   });
 }
